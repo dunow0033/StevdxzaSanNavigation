b4fa53e7de93696a180bc4bd9067ccb57eecea40
Brian Donnoe <dunow33@hotmail.com>
2022-03-11 23:43:18 -0500

    I think I'm finished

---
+++ app/src/main/java/com/express/android/stevdzasannavigation/FirstFragment.kt
@@ -0,0 +1,44 @@
+package com.express.android.stevdzasannavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.express.android.stevdzasannavigation.databinding.ActivityMainBinding
+import com.express.android.stevdzasannavigation.databinding.FragmentFirstBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+
+    private lateinit var _binding: FragmentFirstBinding
+    private val binding: FragmentFirstBinding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentFirstBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+            binding.textView1.setOnClickListener {
+                Navigation.findNavController(binding.root).navigate(R.id.navigateToSecondFragment)
+            }
+    }
+}
---
+++ app/src/main/java/com/express/android/stevdzasannavigation/SecondFragment.kt
@@ -0,0 +1,44 @@
+package com.express.android.stevdzasannavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.express.android.stevdzasannavigation.databinding.FragmentFirstBinding
+import com.express.android.stevdzasannavigation.databinding.FragmentSecondBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+
+    private lateinit var _binding: FragmentSecondBinding
+    private val binding: FragmentSecondBinding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSecondBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textView2.setOnClickListener {
+                Navigation.findNavController(binding.root).navigate(R.id.navigateToFirstFragment)
+            }
+    }
+}
